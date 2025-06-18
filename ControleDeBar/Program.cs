6cf6f2cf5db0e701359e7b0a69ec955c0af532ad
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-06-17 21:44:44 -0300

    Iniciado o projeto Controle de Bar

---
+++ ControleDeBar/Program.cs
@@ -0,0 +1,39 @@
+﻿using ControleDeBar.ConsoleApp.Compartilhado;
+using ControleDeBar.ConsoleApp.ModuloMesa;
+
+namespace ControleDeBar.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TelaPrincipal telaPrincipal = new TelaPrincipal();
+
+            while (true)
+            {
+                telaPrincipal.ApresentarMenuPrincipal();
+
+                ITela telaEscolhida = telaPrincipal.ObterTela();
+
+                if (telaEscolhida == null)
+                    break;
+
+                char opcaoEscolhida = telaEscolhida.ApresentarMenu();
+
+                if (char.ToUpper(opcaoEscolhida) == 'S')
+                    break;
+
+                switch (opcaoEscolhida)
+                {
+                    case '1': telaEscolhida.CadastrarRegistro(); break;
+
+                    case '2': telaEscolhida.EditarRegistro(); break;
+
+                    case '3': telaEscolhida.ExcluirRegistro(); break;
+
+                    case '4': telaEscolhida.VisualizarRegistros(true); break;
+                }
+            }
+        }
+    }
+}
