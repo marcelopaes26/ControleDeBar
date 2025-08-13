a5567e0cff9aa216eb005f1b4c1c6ad5e2a6d4d6
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-08-12 21:53:59 -0300

    Adicionada responsividade no Módulo Contas e imagem na HomePage

--- 1. Apresentação/ControleDeBar.WebApp/obj/Debug/net8.0/ControleDeBar.WebApp.AssemblyInfo.cs
+++ 1. Apresentação/ControleDeBar.WebApp/obj/Debug/net8.0/ControleDeBar.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeBar.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a7d899314d4ff51fc35cf77f21fef24adc4096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29b7469ea8ac9daba247a85239a309255353e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeBar.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleDeBar.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
