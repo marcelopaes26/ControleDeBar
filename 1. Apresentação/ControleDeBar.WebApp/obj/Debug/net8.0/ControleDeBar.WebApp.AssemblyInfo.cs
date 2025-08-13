ea9aeb8cf77ee35fe1e9a9fb5326c422333d20f9
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-08-12 21:37:32 -0300

    Iniciados os ajustes de responsividade

--- 1. Apresentação/ControleDeBar.WebApp/obj/Debug/net8.0/ControleDeBar.WebApp.AssemblyInfo.cs
+++ 1. Apresentação/ControleDeBar.WebApp/obj/Debug/net8.0/ControleDeBar.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeBar.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476adb6367f0522c2f261fe4ee46b15937b90c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a7d899314d4ff51fc35cf77f21fef24adc4096")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeBar.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleDeBar.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
